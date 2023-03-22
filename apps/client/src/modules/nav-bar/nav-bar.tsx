5d2bed630a4ed24be7b6ec63fe2319f28dcc96b9
DinGo <ivan.nesterenko@ukr.net>
2023-03-22 19:36:39 +0200

    created nav-bar

---
+++ apps/client/src/modules/nav-bar/nav-bar.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+
+import { LeftBattleMenu } from '../../../resources/svg/button-svg/left-button-battle-menu';
+import { LeftLeaderboardMenu } from '../../../resources/svg/button-svg/left-button-leaderboard-menu';
+import { LeftProfileMenu } from '../../../resources/svg/button-svg/left-button-profile-menu';
+import { LeftSettingsMenu } from '../../../resources/svg/button-svg/left-button-settings-menu';
+import { NavUserLink } from '../../components/nav-user-link-component/navigation-user-link-component';
+import { RoutesEnum } from '../../routes/app-route/app-route-enums';
+import styles from './nav-bar.module.css';
+
+export const NavBar: FC = () => {
+  return (
+    <nav className={styles.nav}>
+      <NavUserLink navLink={RoutesEnum.Home}>
+        <LeftBattleMenu />
+      </NavUserLink>
+      <NavUserLink navLink={RoutesEnum.Profile}>
+        <LeftProfileMenu />
+      </NavUserLink>
+      <NavUserLink navLink={RoutesEnum.Stats}>
+        <LeftLeaderboardMenu />
+      </NavUserLink>
+      <NavUserLink navLink={RoutesEnum.Settings}>
+        <LeftSettingsMenu />
+      </NavUserLink>
+    </nav>
+  );
+};
