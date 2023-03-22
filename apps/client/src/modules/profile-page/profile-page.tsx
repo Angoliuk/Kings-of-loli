37811c2dcad4ce1d156971a24eb8ac37519ca6c1
DinGo <ivan.nesterenko@ukr.net>
2023-03-22 19:34:43 +0200

    created profile page

---
+++ apps/client/src/modules/profile-page/profile-page.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+
+import { NavigationPageWrapper } from '../../components/navigation-page-wrapper-component';
+import styles from './profile-page.module.css';
+
+export const ProfilePage: FC = () => {
+  return (
+    <NavigationPageWrapper>
+      <div className={styles.container}>
+        <div className={styles.userContainer}>
+          <div className={styles.userProfile}></div>
+          <div className={styles.username}>username</div>
+          <div className={styles.rating}>rating</div>
+        </div>
+        <button className={styles.playButton}>Logout</button>
+      </div>
+    </NavigationPageWrapper>
+  );
+};
