fe435ad35b21c29a90a2464ab22c7d0b11ff0181
DinGo <ivan.nesterenko@ukr.net>
2023-03-22 19:43:39 +0200

    created authorization constans

---
+++ apps/client/src/constants/authorization-consts/authorization-consts.ts
@@ -0,0 +1,26 @@
+const SIGN_IN_FORM_CONSTANTS = {
+  L_Nickname: `Your Nickname is too large`,
+  S_Nickname: `Your Nickname is too small`,
+  S_Paswword: `You need longer password`,
+  Is_Loading: `Checking your nickname`,
+  Is_Error: `Your nickname already use`,
+} as const;
+
+const LOG_IN_FORM_CONSTANTS = {
+  L_Nickname: `Your Nickname is not that long`,
+  S_Nickname: `Your Nickname is definitely longer`,
+  S_Paswword: `Your Password is definitely longer`,
+  Is_Loading: `Checking your data`,
+  Is_Error: `Incorect data`,
+} as const;
+
+enum SignInFormInputState {
+  nickname = 'nickname',
+  password = 'password',
+}
+enum LogInFormInputState {
+  nickname = 'nickname',
+  password = 'password',
+}
+
+export { LOG_IN_FORM_CONSTANTS, LogInFormInputState, SIGN_IN_FORM_CONSTANTS, SignInFormInputState };
