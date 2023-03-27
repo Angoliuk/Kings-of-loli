9dd8b570c57f2b4b9cf8630bab45c20acefcbc4a
DinGo <ivan.nesterenko@ukr.net>
2023-03-27 11:30:19 +0300

    fixed issue with incorect file name

--- apps/client/src/modules/stats-page/user-position.tsx
+++ apps/client/src/modules/stats-page/user-position.tsx
@@ -1,8 +1,8 @@
 import { FC } from 'react';
 
 import { UserPositionProperties } from '../../interfaces/page-interfaces/page-interfaces';
-import styles from './user-possition.module.css';
-export const UserPossition: FC<UserPositionProperties> = ({ user }) => {
+import styles from './user-position.module.css';
+export const UserPosition: FC<UserPositionProperties> = ({ user }) => {
   const { score, name, place } = user;
   return (
     <div className={styles.player}>
