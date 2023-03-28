895684256334b978318d249ed379555ec7b90686
DinGo <ivan.nesterenko@ukr.net>
2023-03-28 11:44:52 +0300

    fixed problem with wrong variable name

--- apps/client/src/modules/settings-page/settings-page.tsx
+++ apps/client/src/modules/settings-page/settings-page.tsx
@@ -8,24 +8,18 @@ export const SettingsPage: FC = () => {
       <div className={styles.container}>
         <div className={styles.menuBar}>
           <h1 className={styles.projectName}>Settings</h1>
-          <div>
-            <div className={styles.prop}>
+          <div className={styles.settings}>
+            <div className={styles.volumeLine}>
               <span>Volume</span>
-              <div className={styles.volumeLine}>
-                <input type="range" />
-              </div>
+              <input type="range" />
             </div>
-            <div className={styles.prop}>
+            <div className={styles.volumeLine}>
               <span>SFX</span>
-              <div className={styles.volumeLine}>
-                <input type="range" />
-              </div>
+              <input type="range" />
             </div>
-            <div className={styles.prop}>
+            <div className={styles.volumeLine}>
               <span>Music</span>
-              <div className={styles.volumeLine}>
-                <input type="range" />
-              </div>
+              <input type="range" />
             </div>
           </div>
         </div>
