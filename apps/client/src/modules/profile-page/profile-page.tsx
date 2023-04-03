5ec24b144367eae273b0ee844ae3c961ba296111
DinGo <ivan.nesterenko@ukr.net>
2023-04-03 11:54:25 +0300

    added pngs

--- apps/client/src/modules/profile-page/profile-page.tsx
+++ apps/client/src/modules/profile-page/profile-page.tsx
@@ -1,19 +1,45 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 import { NavigationPageWrapper } from '../../components/navigation-page-wrapper/navigation-page-wrapper';
 import styles from './profile-page.module.css';
 
 export const ProfilePage: FC = () => {
+  const [visibleProfile, setvisibleProfile] = useState(true);
+  const [visibleLogout, setvisibleLogout] = useState(true);
+  const hoverProfileHandler = () => {
+    setvisibleProfile((previous) => !previous);
+  };
+  const hoverLogoutHandler = () => {
+    setvisibleLogout((previous) => !previous);
+  };
+
   return (
     <NavigationPageWrapper>
       <div className={styles.bg}>
         <div className={styles.content}>
           <div className={styles.userContainer}>
-            <div className={styles.userProfile}></div>
+            <div
+              className={styles.userProfile}
+              onMouseOver={hoverProfileHandler}
+              onFocus={hoverProfileHandler}
+              onMouseOut={hoverProfileHandler}
+              onBlur={hoverProfileHandler}
+            >
+              <div className={styles.userProfileActive} hidden={visibleProfile}></div>
+            </div>
             <div className={styles.username}>username</div>
             <div className={styles.rating}>rating</div>
           </div>
-          <button className={styles.logoutButton}>Logout</button>
+          <button
+            className={styles.logoutButton}
+            onMouseOver={hoverLogoutHandler}
+            onFocus={hoverLogoutHandler}
+            onMouseOut={hoverLogoutHandler}
+            onBlur={hoverLogoutHandler}
+          >
+            Logout
+            <div className={styles.logoutButtonActive} hidden={visibleLogout}></div>
+          </button>
         </div>
       </div>
     </NavigationPageWrapper>
