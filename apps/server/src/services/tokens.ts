80a76f1ef9c98d13b8a1d6035738160485ef7c44
Angoliuk <gamongara@gmail.com>
2023-04-14 10:04:41 +0300

    using only cookies token

--- apps/server/src/services/tokens.ts
+++ apps/server/src/services/tokens.ts
@@ -51,14 +51,8 @@ export const signTokens = async (user: Omit<User, 'password'>) => {
 
 export const getHeaderUser = async ({ req }: CreateExpressContextOptions) => {
   try {
-    const { access_token: cookiesAccessToken } = req.cookies as { access_token?: string };
+    const { access_token } = req.cookies as { access_token?: string };
 
-    let access_token;
-    if (req.headers.authorization) {
-      access_token = req.headers.authorization;
-    } else if (cookiesAccessToken) {
-      access_token = cookiesAccessToken;
-    }
     if (typeof access_token !== 'string') {
       return;
     }
