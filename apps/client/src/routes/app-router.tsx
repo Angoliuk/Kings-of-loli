9587c30324e223d86bfec480bd8c7ffd08d3d99c
Angoliuk <gamongara@gmail.com>
2023-04-22 17:20:32 +0300

    new route for leaderboard

--- apps/client/src/routes/app-router.tsx
+++ apps/client/src/routes/app-router.tsx
@@ -2,11 +2,11 @@ import { FC } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import { HomePage } from '../modules/home-page/home-page';
+import { LeaderboardContainer } from '../modules/leaderboard/leaderboard-container';
 import { ProfilePage } from '../modules/profile-page/profile-page';
 import { SettingsPage } from '../modules/settings-page/settings-page';
 import { SignIn } from '../modules/sign-in-page/sign-in-page';
 import { SignUp } from '../modules/sign-up-page/sign-up-page';
-import { StatsPage } from '../modules/stats-page/stats-page';
 import { useAuthStore } from '../store/auth-store/auth-store';
 import styles from './app-route.module.css';
 import { AppRoutes } from './app-router-enum';
@@ -22,7 +22,7 @@ export const AppRouter: FC = () => {
             <Route path={AppRoutes.Home} element={<HomePage />} />
             <Route path={AppRoutes.Base} element={<Navigate to={AppRoutes.Home} />} />
             <Route path={AppRoutes.Profile} element={<ProfilePage />} />
-            <Route path={AppRoutes.Stats} element={<StatsPage />} />
+            <Route path={AppRoutes.Stats} element={<LeaderboardContainer />} />
             <Route path={AppRoutes.Settings} element={<SettingsPage />} />
           </Route>
           <Route path={AppRoutes.SignUp} element={<SignUp />} />
