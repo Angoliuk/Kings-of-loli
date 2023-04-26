a50232ef3b7321b433afc7a0b7904a841c3598ab
Angoliuk <gamongara@gmail.com>
2023-04-26 16:05:42 +0300

    better redis connection

--- apps/server/src/database/redis.ts
+++ apps/server/src/database/redis.ts
@@ -1,5 +1,18 @@
 import Redis from 'ioredis';
 
-export const redisClient = new Redis(process.env.REDIS_DATABASE_URL);
-redisClient.on('connect', () => console.log('🚀 Redis client connected...'));
-redisClient.on('error', (error) => console.error('Redis Client error:', '\n', error));
+// for some reasons process.env.REDIS_DATABASE_URL can be undefined,
+// event if we provided one, so this function just trying to reconnect until success
+const connectRedis = () => {
+  if (!process.env.REDIS_DATABASE_URL) {
+    console.warn('no REDIS_DATABASE_URL found, retrying');
+    setTimeout(connectRedis, 10);
+    return;
+  }
+  const redis = new Redis(process.env.REDIS_DATABASE_URL);
+  redis.on('connect', () => console.log('🚀 Redis client connected...'));
+  redis.on('error', (error) => console.error('Redis Client error:', '\n', error));
+
+  return redis;
+};
+
+export const redisClient = connectRedis();
