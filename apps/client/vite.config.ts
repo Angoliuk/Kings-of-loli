3b743c84286c9e85e02ed7e035aa5c1f2d61ce4a
Angoliuk <gamongara@gmail.com>
2023-04-27 19:11:57 +0300

    correct env handling

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,26 +1,30 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: Number(process.env.FRONTEND_PORT),
-    hmr: {
-      host: 'localhost',
+export default ({ mode }: { mode: string }) => {
+  const environment = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    server: {
+      host: '0.0.0.0',
+      port: Number(environment.VITE_FRONTEND_PORT),
+      hmr: {
+        host: 'localhost',
+      },
+      watch: {
+        usePolling: true,
+      },
     },
-    watch: {
-      usePolling: true,
+    plugins: [
+      react({
+        include: '**/*.tsx',
+      }),
+    ],
+    css: {
+      modules: {
+        localsConvention: 'camelCase',
+      },
     },
-  },
-  plugins: [
-    react({
-      include: '**/*.tsx',
-    }),
-  ],
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-    },
-  },
-});
+  });
+};
