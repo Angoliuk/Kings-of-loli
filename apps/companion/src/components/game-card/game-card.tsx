354a654570c0e6b476fedde7b9aee0f806726ba4
Angoliuk <gamongara@gmail.com>
2023-05-02 10:54:17 +0300

    adaptive design for game card

--- apps/companion/src/components/game-card/game-card.tsx
+++ apps/companion/src/components/game-card/game-card.tsx
@@ -26,27 +26,29 @@ const DeletedGameUserStats: FC = () => {
 const GameUserStats: FC<GameUserStatsProps> = ({ userGameStats }) => {
   const scoreGained = Math.random() > 0.5 ? Math.floor(Math.random() * 15) : -Math.floor(Math.random() * 15);
   return userGameStats ? (
-    <Link className="w-2/6" href={`/users-stats/${userGameStats.userId}`}>
-      <div className="flex items-center rounded-lg bg-zinc-700/60 p-5">
+    <Link className="w-full md:w-5/12 xl:w-2/6" href={`/users-stats/${userGameStats.userId}`}>
+      <div className="flex flex-row rounded-lg bg-zinc-700/60 p-5 md:flex-col lg:flex-row lg:items-center">
         <Image
           alt=""
           src={'https://picsum.photos/200/300'}
-          width={100}
-          height={100}
-          className="h-20 w-20 rounded-full"
+          width={80}
+          height={80}
+          className="h-20 w-20 self-center rounded-full"
         />
-        <div className="pl-5">
-          <div className="flex justify-between">
+        <div className="pl-5 md:pl-0 lg:pl-5">
+          <div className="mt-3 flex">
             <SVGIcon name="userIcon" width={16} height={16} />
-            <p className="pl-5 text-slate-200">{userGameStats.user.name}</p>
+            <p className="truncate pl-5 text-xl text-slate-200">{userGameStats.user.name}Some longer name</p>
           </div>
-          <div className="flex justify-between">
-            <SVGIcon name="cupIcon" width={16} height={16} />
-            <p className="pl-5 text-slate-200">{userGameStats.user.score}</p>
-          </div>
-          <div className="flex justify-between">
-            <SVGIcon name="medalIcon" width={16} height={16} />
-            <p className={`pl-3 ${scoreGained > 0 ? 'text-green-500' : 'text-rose-500'}`}>{scoreGained}</p>
+          <div className="flex justify-between md:mt-2 md:flex-row lg:mt-0 lg:flex-col">
+            <div className="flex">
+              <SVGIcon name="cupIcon" width={16} height={16} />
+              <p className="truncate pl-5 text-slate-200">{userGameStats.user.score}</p>
+            </div>
+            <div className="flex">
+              <SVGIcon name="medalIcon" width={16} height={16} />
+              <p className={`truncate pl-5 ${scoreGained > 0 ? 'text-green-500' : 'text-rose-500'}`}>{scoreGained}</p>
+            </div>
           </div>
         </div>
       </div>
@@ -57,10 +59,9 @@ const GameUserStats: FC<GameUserStatsProps> = ({ userGameStats }) => {
 };
 export const GameCard: FC<GameCardProps> = ({ game }) => {
   return (
-    // <Link href={`/games-stats/${game.id}`}>
-    <div className="container my-4 flex flex-row items-center justify-between rounded-xl bg-zinc-700/60 px-6 py-4">
+    <div className="my-4 flex w-full flex-col items-center justify-between rounded-xl bg-zinc-700/60 px-6 py-4 md:flex-row">
       <GameUserStats userGameStats={game.usersMatchStats?.[0]} />
-      <div className="flex flex-col">
+      <div className="my-5 flex flex-col md:my-0">
         <div>
           <p className="text-center text-cyan-600">
             {Duration.fromMillis(game.duration * 1000).toFormat("mm 'min' ss 'sec'")}
@@ -80,6 +81,5 @@ export const GameCard: FC<GameCardProps> = ({ game }) => {
       </div>
       <GameUserStats userGameStats={game.usersMatchStats?.[1]} />
     </div>
-    // </Link>
   );
 };
