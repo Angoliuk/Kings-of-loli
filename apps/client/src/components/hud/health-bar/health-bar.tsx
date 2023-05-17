ce817f8a45ae1335747ddb5d340f04dd10240406
DinGo <ivan.nesterenko@ukr.net>
2023-05-17 20:04:14 +0300

    WIP

--- apps/client/src/components/hud/health-bar/health-bar.tsx
+++ apps/client/src/components/hud/health-bar/health-bar.tsx
@@ -2,23 +2,23 @@ import { Sprite } from '@pixi/react';
 
 export const HudHealthBar = ({ hp }: { hp: number }) => {
   const maxHp = 100;
-  const height = -360;
+  const height = -60;
   const hpHeight = (hp / maxHp) * height;
-  const heightTop = 520;
+  const heightTop = 210;
   const hpHeightTop = hpHeight + heightTop;
   return (
     <>
       <Sprite
         y={hpHeightTop}
         anchor={[0, 0]}
-        image={`resources/img/map/hud/health-top.png`}
-        scale={{ x: 5, y: 3 }}
+        image={`resources/img/map/hud/health-top-hd.png`}
+        scale={{ x: 1.2, y: 1 }}
       />
       <Sprite
         y={530}
         anchor={[0, 0]}
-        image={`resources/img/map/hud/health.png`}
-        scale={{ x: 5, y: hpHeight }}
+        image={`resources/img/map/hud/health-hd.png`}
+        scale={{ x: 1.2, y: hpHeight }}
       />
     </>
   );
